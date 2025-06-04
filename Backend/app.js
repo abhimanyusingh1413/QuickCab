cc57f58d145e3650f8fcb85d02ea8adf1ad408b2
abhimanyusingh1413 <abhimanyukr1413@gmail.com>
2025-06-04 20:28:28 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,21 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectTODb = require('./db/db.js');
+const userRouter = require('./routes/user.routes.js');
+
+
+connectTODb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
+
+app.use('/users', userRouter);
 
 module.exports = app;
 
---
+++ Backend/db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+function connectTODb(){
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+        console.log('🔗 Connected to DB');
+    }).catch(err => console.log(err));
+}
+
+module.exports = connectTODb;

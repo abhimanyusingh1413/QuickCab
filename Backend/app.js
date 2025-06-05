707a1b2e8155b7a30d3d46c1e769183b83654bba
abhimanyusingh1413 <abhimanyukr1413@gmail.com>
2025-06-05 10:11:35 +0530

    users authentication doen here

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectTODb = require('./db/db.js');
 const userRouter = require('./routes/user.routes.js');
 
@@ -13,6 +14,7 @@ connectTODb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
